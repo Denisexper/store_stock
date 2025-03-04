c0fb858e7fdf7cbd0fad2e91bae34a091306af56
Denisexper <clasic.exper2023@gmail.com>
2025-03-03 21:52:06 -0600

    testing

--- frontend/src/componnents/creaPro.jsx
+++ frontend/src/componnents/creaPro.jsx
@@ -1,86 +1,71 @@
 import { useState } from "react";
 import { Toaster, toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
-function crearPro () {
-
-    const [proData, setProData ] = useState({
+function createProduct() {
+    const [productoData, setProductoData] = useState({
         nombre: "",
         precio: "",
         descripcion: "",
         stock: "",
+        categoria: "",
     })
+    const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setProData({
-            ...proData,
-            [name]: value,
-        });
-    };
+   const handleChange = (e) => {
+    const {name, value} = e.target;
+    setProductoData({
+        ...productoData,
+        [name]: value,
+    })
+   }
 
-    const handleSubmit = async (e) => {
-        console.log(FormData);
-        e.preventDefault();
-        try {
-            const response = await fetch("http://localhost:8080/api/testes", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(proData),
-            });
-            const data = await response.json();
-            console.log(data)
-            if(response.ok){
-                toast.success("producto creado")
-            } else {
-                toast.success("error creando")
-            }
-        } catch (error) {
-            console.error(error)
+   const handleSubmit = async (e) => {
+    e.preventDefautl();
+    try {
+        const response = await fetch("http://localhost:3000/api/creatae", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(productoData)
+        })
+        const data = await response.json();
+        if(response.ok){
+            toast.success("producto creado")
+        } else {
+            toast.success("error creando producot")
         }
+    } catch (error) {
+        console.log(error)
     }
+   }
 
-    return (
-        <div>
-            <h1>Crear producto</h1>
+   const handleList = () => {
+    navigate("/")
+   }
 
-            <form onSubmit={handleSubmit}>
+   return (
+    <div>
+        <form onDoubleClick={handleSubmit}>
+            <input 
+            type="text"
+            name="nombre"
+            value={productoData.nombre}
+            onChange={handleChange}
+            placeholder="nombre producto"
+            className="input" 
+            />
 
-                <input
-                type="text"
-                name= "nombre"
-                value={proData.nombre}
-                onChange={handleChange}
-                placeholder="nombre producto"
-                className="input" 
-                />
-
-                <input 
-                type="text"
-                name="precio"
-                value={proData.precio}
-                onChange={handleChange}
-                placeholder="precio"
-                className="input"
-                />
-
-                <input type="text"
-                name="descripcion"
-                value={proData.descripcion}
-                onChange={handleChange}
-                placeholder="descripcion"
-                className="input"
-                />
-
-                <input type="text"
-                name="stock"
-                value={proData.stock}
-                onChange={handleChange}
-                placeholder="stock"
-                className="input" />
-            </form>
-        </div>
-    )
-
+            <input 
+            type="text"
+            name="precio"
+            value={productoData.precio}
+            onChange={handleChange}
+            placeholder="precio producto"
+            className="input" 
+            />
+        </form>
+    </div>
+   )
 }
